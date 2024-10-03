ebbf93ee5368d82172f285e5e7c850bbbebda6b5
unknown <krkeerthanakr@gmail.com>
2024-10-04 00:01:08 +0530

    search functionaity with active fields and searching with initial letters

--- service_search/models.py
+++ service_search/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 
 class Subcategory(models.Model):
     title = models.CharField(max_length=50, db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
     description = models.TextField(default='No description provided')
     status = models.CharField(max_length=10, choices=[('Active', 'Active'), ('Inactive', 'Inactive')], default='Active')
 
@@ -22,8 +22,9 @@ class Subcategory(models.Model):
 
 class ServiceProvider(models.Model):
     name = models.CharField(max_length=100)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='service_providers')
+    #subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='service_providers')
     description = models.TextField()
+    status = models.CharField(max_length=10, choices=[('Active', 'Active'), ('Inactive', 'Inactive')], default='Active')
 
     def __str__(self):
         return self.name
